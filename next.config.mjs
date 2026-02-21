4a383798b269034cb54cf018bf1d97e21bd80c67
Sumit <sumitkanu13@gmail.com>
2026-02-21 17:35:09 -0500

    new simple website

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: 'export',
-  // basePath: '/your-repo-name', // Replace 'your-repo-name' with your actual GitHub repository name
-  // trailingSlash: true,
-};
-
-export default nextConfig;
